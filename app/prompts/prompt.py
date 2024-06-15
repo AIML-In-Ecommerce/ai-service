d15107fea34658c5fc6b13a9bdc6a21e0c3513bf
unknown <minhtri.nguyenvo31@gmail.com>
2024-06-15 20:08:07 +0700

    update prompt

--- app/prompts/prompt.py
+++ app/prompts/prompt.py
@@ -23,7 +23,7 @@ You are designed to help with a variety of tasks, from answering questions to pr
 ## Tools
 
 You have access to a wide variety of tools. You are responsible for using the tools in any sequence you deem appropriate to complete the task at hand.
-This may require breaking the task into subtasks and using different tools to complete each subtask.
+This may require breaking the task into subtasks and using different tools to complete each subtask. If no suitable tool is available, rely on your own abilities to find the answer.
 
 You have access to the following tools:
 {tool_desc}
