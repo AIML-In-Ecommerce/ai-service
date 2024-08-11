0ac3df9d5de083d17675d9c16b44bd6f8f7cd457
unknown <minhtri.nguyenvo31@gmail.com>
2024-08-11 17:07:06 +0700

    update revenue service

--- app/tools/product_engine.py
+++ app/tools/product_engine.py
@@ -18,5 +18,5 @@ def callProductListApi(request_param_str: str):
 product_engine = FunctionTool.from_defaults(
     fn = callProductListApi,
     name  = "product_getter",
-    description="this tool can get list product by keyword, parameter request_param_str is a URL's query string which provide additional information include keyword (the keyword of product being found bu user), quantity (the number of product user want to receive), sortBy ({{soldQuantity}} in case related to to number of sold product and {{avgRating}} in case related to rating value). Example: {{request_param_str: '?keyword=ao-thun?quantity=2?sortBy=soldQuantity'}}  It's not necessary to always have all three request parameters.",
+    description="this tool can get list product by keyword, parameter request_param_str is a URL's query string which provide additional information include keyword (the keyword of product being found bu user), quantity (the number of product user want to receive), sortBy ({{soldQuantity}} in case related to to number of sold product and {{avgRating}} in case related to rating value). Example: {{request_param_str: '?keyword=ao-thun?quantity=2?sortBy=soldQuantity'}}  It's not necessary to always have all three request parameters. For example, '?shop=123456789&year=2024&month=7'",
 )
